7b932e8ccec7ec8b1f4bdd8825eaa845c1c0777f
PhuocPham930 <phuocpham2411@gmail.com>
2024-02-19 12:09:12 +0700

    C# GUI

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lsbSendData = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbbMode = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbbSpeed = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cbbBaudrate = new System.Windows.Forms.ComboBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbbPort
+            // 
+            this.cbbPort.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbPort.FormattingEnabled = true;
+            this.cbbPort.Location = new System.Drawing.Point(10, 52);
+            this.cbbPort.Name = "cbbPort";
+            this.cbbPort.Size = new System.Drawing.Size(74, 31);
+            this.cbbPort.TabIndex = 1;
+            this.cbbPort.SelectedIndexChanged += new System.EventHandler(this.cbbPort_SelectedIndexChanged);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(6, 159);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 30);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Kết nối";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(117, 158);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 31);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Ngắt";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM9";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnScan);
+            this.groupBox1.Controls.Add(this.cbbBaudrate);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtStatus);
+            this.groupBox1.Controls.Add(this.cbbPort);
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 196);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thiết lập";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.Color.Tomato;
+            this.txtStatus.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(42, 89);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(120, 30);
+            this.txtStatus.TabIndex = 3;
+            this.txtStatus.TabStop = false;
+            this.txtStatus.Text = "Disconected";
+            this.txtStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtStatus.TextChanged += new System.EventHandler(this.txtStatus_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.txtData);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(537, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 189);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Giao tiếp";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(181, 87);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 31);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Xóa";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(63, 87);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 30);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Gửi";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(63, 35);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(193, 27);
+            this.txtData.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lsbSendData
+            // 
+            this.lsbSendData.FormattingEnabled = true;
+            this.lsbSendData.ItemHeight = 23;
+            this.lsbSendData.Location = new System.Drawing.Point(12, 302);
+            this.lsbSendData.Name = "lsbSendData";
+            this.lsbSendData.Size = new System.Drawing.Size(797, 119);
+            this.lsbSendData.TabIndex = 4;
+            this.lsbSendData.TabStop = false;
+            this.lsbSendData.SelectedIndexChanged += new System.EventHandler(this.lsbSendData_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.cbbMode);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.cbbSpeed);
+            this.groupBox3.Location = new System.Drawing.Point(245, 107);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(274, 189);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Hiển thị";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hiệu ứng";
+            // 
+            // cbbMode
+            // 
+            this.cbbMode.FormattingEnabled = true;
+            this.cbbMode.Items.AddRange(new object[] {
+            "Chạy từ phải sang trái",
+            "Chạy từ trái sang phải",
+            "Chữ đứng yên"});
+            this.cbbMode.Location = new System.Drawing.Point(10, 131);
+            this.cbbMode.Name = "cbbMode";
+            this.cbbMode.Size = new System.Drawing.Size(258, 31);
+            this.cbbMode.TabIndex = 2;
+            this.cbbMode.SelectedIndexChanged += new System.EventHandler(this.cbbMode_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tốc độ";
+            // 
+            // cbbSpeed
+            // 
+            this.cbbSpeed.FormattingEnabled = true;
+            this.cbbSpeed.Items.AddRange(new object[] {
+            "mức 1 (nhanh nhất)",
+            "mức 2",
+            "mức 3",
+            "mức 4",
+            "mức 5 (chậm nhất)"});
+            this.cbbSpeed.Location = new System.Drawing.Point(10, 59);
+            this.cbbSpeed.Name = "cbbSpeed";
+            this.cbbSpeed.Size = new System.Drawing.Size(159, 31);
+            this.cbbSpeed.TabIndex = 0;
+            this.cbbSpeed.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(279, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ma trận Led giao tiếp máy tính";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(325, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Đồ Án 2 ĐH Sư Phạm Kỹ Thuật TP.HCM";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(481, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(331, 23);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "SVTH: Phạm Tấn Phước - MSSV: 20161352";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(18, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(257, 23);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "GVHD: Th.S Nguyễn Thanh Bình";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(113, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 23);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Baudrate";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // cbbBaudrate
+            // 
+            this.cbbBaudrate.FormattingEnabled = true;
+            this.cbbBaudrate.Location = new System.Drawing.Point(117, 52);
+            this.cbbBaudrate.Name = "cbbBaudrate";
+            this.cbbBaudrate.Size = new System.Drawing.Size(82, 31);
+            this.cbbBaudrate.TabIndex = 5;
+            this.cbbBaudrate.SelectedIndexChanged += new System.EventHandler(this.cbbBaudrate_SelectedIndexChanged);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.Location = new System.Drawing.Point(63, 123);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(74, 29);
+            this.btnScan.TabIndex = 6;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 433);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.lsbSendData);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lsbSendData;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox cbbSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbbMode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cbbBaudrate;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            string[] baudrate = { "1200", "2400", "4800", "9600", "115200" };
+            cbbPort.Items.AddRange(ports);
+            cbbBaudrate.Items.AddRange(baudrate);
+            btnClose.Enabled = false;
+            cbbSpeed.SelectedIndex = 2;
+            cbbMode.SelectedIndex = 0;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if(cbbPort.Text=="")
+            {
+                MessageBox.Show("Chưa chọn COM Port","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            else if(cbbBaudrate.Text=="")
+            {
+                MessageBox.Show("Chưa chọn Baudrate", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    if(serialPort1.IsOpen)
+                    {
+                        MessageBox.Show("Port đang kết nối", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        serialPort1.Open();
+                        txtStatus.BackColor = Color.Lime;
+                        txtStatus.Text = "Conecting...";
+                        cbbPort.Enabled = false;
+                        btnClose.Enabled = true;
+                        btnOpen.Enabled = false;
+                        cbbBaudrate.Enabled = false;
+                        btnScan.Enabled = false;
+                        MessageBox.Show("Port: " + serialPort1.PortName + " " + serialPort1.BaudRate);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Không tìm được COM Port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    txtStatus.BackColor = Color.Red;
+                    txtStatus.Text = "Disconected";
+                    cbbPort.Enabled = true;
+                    btnClose.Enabled = false;
+                    btnOpen.Enabled = true;
+                    cbbBaudrate.Enabled = true;
+                    btnScan.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Chưa kết nối với Port nào cả!!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Không tìm được COM Port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        private void cbbPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = cbbPort.Text;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string time = dateTime.ToString("HH:mm:ss");
+            string date = dateTime.ToString("dd/MM/yyyy");
+            string chuoi = "";
+            if(cbbMode.SelectedIndex == 0)       chuoi = txtData.Text + '0';
+            else if(cbbMode.SelectedIndex == 1)  chuoi = txtData.Text + '1';
+            else                                 chuoi = txtData.Text + '2';
+
+            if (cbbSpeed.SelectedIndex == 0)     chuoi += '1';
+            else if(cbbSpeed.SelectedIndex == 1) chuoi += '2';
+            else if (cbbSpeed.SelectedIndex == 2)chuoi += '3';
+            else if (cbbSpeed.SelectedIndex == 3)chuoi += '4';
+            else                                 chuoi += '5';
+
+            if (serialPort1.IsOpen == true)
+            {
+                if(cbbMode.SelectedIndex != 2)
+                {
+                    lsbSendData.Items.Add(date + "  " + time + "\t" +
+                                          txtData.Text + "\t" +
+                                          "Hiệu ứng: " + cbbMode.SelectedItem + '\t' +
+                                          "Tốc độ chạy chữ: " + cbbSpeed.SelectedItem);
+                }
+                else
+                {
+                    lsbSendData.Items.Add(date + "  " + time + "\t" +
+                      txtData.Text + "\t" +
+                      "Hiệu ứng: " + cbbMode.SelectedItem);
+                } 
+                serialPort1.Write(chuoi);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtData.Clear();
+        }
+
+        private void txtStatus_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbbMode.SelectedIndex == 2)
+                cbbSpeed.Enabled = false;
+            else
+                cbbSpeed.Enabled = true;
+        }
+
+        private void lsbSendData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbBaudrate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = Convert.ToInt32(cbbBaudrate.Text);
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            cbbPort.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            cbbPort.Items.AddRange(ports);
+        }
+    }
+}
